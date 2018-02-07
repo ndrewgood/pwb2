d4958fbb4d80299c352e95f946c4aa402ca2bb2b
Andrew G <gandrew888@gmail.com>
2018-02-07 16:49:38 -0500

    added a top button and changed back button

--- js/index.js
+++ js/index.js
@@ -5,3 +5,20 @@ var animation = bodymovin.loadAnimation({
   autoplay: true,
   path: 'js/data.json'
 })
+
+// When the user scrolls down 300px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        document.getElementById("top-button").style.display = "block";
+    } else {
+        document.getElementById("top-button").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
