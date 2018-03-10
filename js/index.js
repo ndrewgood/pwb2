b6a282c86715f19657168bcf258c798d4600e488
Andrew G <gandrew888@gmail.com>
2018-03-09 22:39:30 -0500

    Happy Birthday Jack!!!!!!

--- js/index.js
+++ js/index.js
@@ -6,6 +6,15 @@ var animation = bodymovin.loadAnimation({
   path: 'js/data.json'
 })
 
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('jbd'),
+  renderer: 'svg',
+  loop: false,
+  autoplay: true,
+  path: 'design/jbd/data.json'
+})
+
+
 // When the user scrolls down 300px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
